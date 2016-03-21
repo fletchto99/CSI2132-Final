eb461e2afc472e92ef0808edb9027f9e94d25959
Matt Langlois <fletchto99@gmail.com>
2016-03-21 16:11:25 -0400

    some debug logging

--- backend/database.js
+++ backend/database.js
@@ -8,12 +8,14 @@ exports.connect = function(connection, schema, done) {
     var database = new postgres.Client(connection);
     database.connect(function(error) {
         if (error) {
+            console.log("Error connecting to database: " + error);
             done(error)
         } else {
             state.connection = database;
             if (schema) {
                 state.connection.query('set search_path='+schema);
             }
+            console.log("Connection to database successful!");
             done();
         }
 
