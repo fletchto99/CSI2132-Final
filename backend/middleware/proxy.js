22c41428e768185b9396161ff2c549b5890e83da
Matt Langlois <fletchto99@gmail.com>
2016-03-21 17:13:15 -0400

    Target localhost

--- backend/middleware/proxy.js
+++ backend/middleware/proxy.js
@@ -1,7 +1,7 @@
 var proxyMiddleware = require('http-proxy-middleware');
 
 module.exports = proxyMiddleware('/api', {
-    target: '127.0.0.1:8080',
+    target: 'http://localhost:8080',
     changeOrigin: true,
     ws: true,
     pathRewrite: {
