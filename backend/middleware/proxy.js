730511db3e26bd37060ae38be041cacf48febe8c
Matt Langlois <fletchto99@gmail.com>
2016-03-21 17:10:31 -0400

    Fix proxy middleware

--- backend/middleware/proxy.js
+++ backend/middleware/proxy.js
@@ -1,12 +1,10 @@
 var proxyMiddleware = require('http-proxy-middleware');
-var express = require('express');
-var router = express.Router();
 
-router.use(proxyMiddleware('/api', {
+modules.exports = proxyMiddleware('/api', {
     target: '127.0.0.1:8080',
     changeOrigin: true,
     ws: true,
     pathRewrite: {
         '^/remove/api' : ''
     }
-}));
+});
