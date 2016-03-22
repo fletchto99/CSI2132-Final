87ef14d77d9c2f0d27270c37bbbf3c36a7ac9af3
Matt Langlois <fletchto99@gmail.com>
2016-03-21 21:42:53 -0400

    remove reference to basic-auth

--- backend/app/middleware/auth.js
+++ backend/app/middleware/auth.js
@@ -1,12 +1,10 @@
 module.exports = function (req, res, next) {
-    var user = auth(req);
     if (!req.session || !req.session.name || !req.session.pass) {
         res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
         res.sendStatus(401);
         return;
     }
     if (req.session.name === 'test' && req.session.pass === 'test') {
-        req.username = user.name;
         next();
     } else {
         res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
