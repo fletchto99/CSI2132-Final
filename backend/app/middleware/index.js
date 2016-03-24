e17955b4dbc2ace35d5b504367445f73e2ff8583
Matt Langlois <fletchto99@gmail.com>
2016-03-24 15:36:10 -0400

    Use helmet

--- backend/app/middleware/index.js
+++ backend/app/middleware/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var helmet = require('helmet');
 var router = express.Router();
 
 router.use(require('./proxy'));
 router.use(bodyParser.json());
+router.use(helmet());
 router.use(session(require('../../config/session.json')));
 router.use('/auth', require('./auth'));
 
