5b6d56fd70ccc74bc10503a6585deee7acc3b0a0
Matt Langlois <fletchto99@gmail.com>
2016-03-25 16:03:15 -0400

    Logout should be a POST

--- backend/app/controllers/session/logout.js
+++ backend/app/controllers/session/logout.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (request) {
+router.post('/', function (request) {
     request.session.destroy();
 });
 
