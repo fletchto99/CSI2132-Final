c7e9eb0f5a2961b7c13f229ca90d7b723a8868bc
Matt Langlois <fletchto99@gmail.com>
2016-03-24 21:55:54 -0400

    Fix more typos

--- backend/app/controllers/session/create.js
+++ backend/app/controllers/session/create.js
@@ -12,10 +12,10 @@ router.put('/', function (req, res) {
             username: req.body.username,
             password: req.body.password
         }).then(function (result) {
-            request.session.user = result;
-            response.json(request.session.user);
+            req.session.user = result;
+            res.json(req.session.user);
         }, function (error) {
-            response.status(400).json(error);
+            res.status(400).json(error);
         });
     }
 });
